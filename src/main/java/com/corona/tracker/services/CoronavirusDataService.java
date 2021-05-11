8c99b8f056e9e3682d63318ad55f294d2e27d292
Udara Madhushan <udaram188@gmail.com>
2021-05-11 19:30:45 +0530

    url change

--- src/main/java/com/corona/tracker/services/CoronavirusDataService.java
+++ src/main/java/com/corona/tracker/services/CoronavirusDataService.java
@@ -9,6 +9,8 @@ import javax.annotation.PostConstruct;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -18,76 +20,67 @@ import com.corona.tracker.models.Stats;
 @Service
 public class CoronavirusDataService {
 
-
-
-		
-		Stats newStats = new Stats();
+	@Value(value = "web.url1")
+	String webUrl1;
 	
-		
-		@PostConstruct
-		@Scheduled(cron = "* * 1 * * *")
-		public void fetchData () throws IOException, InterruptedException {
-			
-			
-			
-			Document webDoc = Jsoup.connect("https://www.hpb.health.gov.lk/en").get();
-			Document webDoc2 = Jsoup.connect("https://www.worldometers.info/coronavirus/").get(); 
-			
-			String confirmedLocal = webDoc.select("p:contains(Total Confirmed Cases)")
-					.first().parent()
-					.selectFirst(".local").select("h4").first().attr("data-counter");
-			
-			String confirmedGlobal = webDoc.select("p:contains(Total Confirmed Cases)")
-					.first().parent().selectFirst(".global")
-					.select("h4").first().attr("data-counter");
-			 String activeLocal = webDoc.select("p:contains(Active Cases)")
-						.first().parent()
-						.selectFirst(".local").select("h4").first().attr("data-counter");
-//		
-			 String dailyLocal = webDoc.select("p:contains(Daily New Cases)")
-						.first().parent()
-						.selectFirst(".local").select("h4").first().attr("data-counter");
+	@Value(value = "web.url2")
+	String webUrl2;
 
-			 String deathsLocal = webDoc.select("p:contains(Deaths)")
-						.first().parent()
-						.selectFirst(".local").select("h4").first().attr("data-counter");
-			 
-			 
-		Element deathsGlobal =  webDoc2.select("p:contains(Deaths:)")
-						.first().parent()
-						.selectFirst(".maincounter-number").select("span").first();
-//		
-//			 String dailyGlobal = webDoc.select("p:contains(Daily New Cases)")
-//				.first().parent()
-//				.selectFirst(".global").select("h4").first().attr("data-counter");
-			 
-			 
-			 
-				//newStats.setDailyGlobal(dailyGlobal );
-			//newStats.setDeathsGlobal(deathsGlobal );
+	Stats newStats = new Stats();
+
+
+	@PostConstruct
+	@Scheduled(cron = "* * 1 * * *")
+	public void fetchData () throws IOException, InterruptedException {
+
 		
-		System.out.println(deathsGlobal.html());
-			newStats.setConfirmedGlobal(confirmedGlobal);
-			newStats.setConfirmedLocal(confirmedLocal);
-			 newStats.setDailyLocal(dailyLocal);
-		
-			newStats.setDeathsLocal(deathsLocal );
-			
-			 newStats.setActiveLocal (activeLocal );
-			
-		 System.out.println(newStats.toString());
-			
-			 
+		Document webDoc = Jsoup.connect(webUrl1).get();
+		Document webDoc2 = Jsoup.connect(webUrl2).get(); 
+
+		String confirmedLocal = webDoc.select("p:contains(Total Confirmed Cases)")
+				.first().parent()
+				.selectFirst(".local").select("h4").first().attr("data-counter");
+
+		String confirmedGlobal = webDoc.select("p:contains(Total Confirmed Cases)")
+				.first().parent().selectFirst(".global")
+				.select("h4").first().attr("data-counter");
+		String activeLocal = webDoc.select("p:contains(Active Cases)")
+				.first().parent()
+				.selectFirst(".local").select("h4").first().attr("data-counter");
+		//		
+		String dailyLocal = webDoc.select("p:contains(Daily New Cases)")
+				.first().parent()
+				.selectFirst(".local").select("h4").first().attr("data-counter");
+
+		String deathsLocal = webDoc.select("p:contains(Deaths)")
+				.first().parent()
+				.selectFirst(".local").select("h4").first().attr("data-counter");
+
+
+		String deathsGlobal =  webDoc2.select("#maincounter-wrap").eq(1).select("span").text();
+
+
+		newStats.setConfirmedGlobal(confirmedGlobal);
+		newStats.setConfirmedLocal(confirmedLocal);
+		newStats.setDailyLocal(dailyLocal);
+
+		newStats.setDeathsLocal(deathsLocal );
+
+		newStats.setActiveLocal (activeLocal );
+
+
+
+
 	}
 
-		
-
-	
-
-		public Stats getNewStats() {
-			return newStats;
-		}
 
 
-	
+
+
+	public Stats getNewStats() {
+		return newStats;
+	}
+
+
+
 }
