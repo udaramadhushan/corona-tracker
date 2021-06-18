96e881d749bb7c05bb02a2f9f8ff5df31883d2a5
Udara Madhushan <udaram188@gmail.com>
2021-06-18 20:04:51 +0530

    changes

--- src/main/java/com/corona/tracker/CoronaTrackerApplication.java
+++ src/main/java/com/corona/tracker/CoronaTrackerApplication.java
@@ -4,7 +4,7 @@ package com.corona.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
 
 
 
